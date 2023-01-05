97d756a432c7989f13121d69c36353df64c95ce3
TonnyOliveirant <98629251+TonnyOliveirant@users.noreply.github.com>
2023-01-04 21:13:44 -0300

    Update Exercicios3.java

--- Exercicios3.java
+++ Exercicios3.java
@@ -53,3 +53,43 @@ public class Exercicios03{
     sc.close();
     }
 } */
+
+
+/* import java.util.Locale;
+import java.util.Scanner;
+
+
+
+public class Exercicios03{
+    public static void main(String[]args){
+        int x, qtyAlcool, qtyGas, qtyDiesel;
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        qtyAlcool = 0;
+        qtyGas = 0;
+        qtyDiesel = 0;
+        System.out.printf("Reference Table:\n1- Alcool\n2- Gasolina\n3- Diesel\n4- Fim");
+        x = sc.nextInt();
+        while ((x > 0) && (x < 4)){
+            switch (x) {
+                case 1:
+                    qtyAlcool+=1;
+                    break;
+                case 2:
+                    qtyGas+=1;
+                    break;
+                case 3:
+                    qtyDiesel+=1;
+                    break;
+                case 4:
+                    System.out.printf("Operation Finished");
+                    break;
+                default:
+                    System.out.printf("Not Supported Number");
+            }
+            x = sc.nextInt();
+        }
+        sc.close();
+        System.out.printf("Thank You\nAlcool: %d\nGasoline: %d\nDiesel: %d", qtyAlcool, qtyGas, qtyDiesel);
+    }
+} */
