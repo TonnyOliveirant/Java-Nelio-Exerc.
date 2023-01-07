86075dab23d5f347f8dc5bdb9895cdcab6e9d36b
TonnyOliveirant <98629251+TonnyOliveirant@users.noreply.github.com>
2023-01-07 14:01:03 -0300

    Create POOExec.java

---
+++ POOExec.java
@@ -0,0 +1,56 @@
+/*package Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entities.employee;
+
+public class Exercicios2 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        employee emp = new employee();
+
+        emp.name = sc.nextLine();
+        emp.salary = sc.nextDouble();
+        emp.tax = sc.nextDouble();
+
+        System.out.printf("Employee: %s, $ %.2f\n", emp.name, emp.liquidsalary());
+
+        System.out.print("Which percentage to increase salary? ");
+        emp.percentageIncrement = sc.nextDouble();
+        System.out.printf("\n Updated data: %s, $ %.2f", emp.name, emp.increasedsalary());
+
+        sc.close();
+
+        
+    }
+}*/
+
+/* package Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import Entities.Rectangle;
+
+public class Exercicios1 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rect = new Rectangle();
+
+        System.out.println("Enter the value of width: ");
+        rect.width = sc.nextDouble();
+        System.out.println("Enter the value of height: ");
+        rect.height = sc.nextDouble();
+
+        System.out.printf("The sum of width and height is: %.2f\n", rect.width + rect.height);
+        System.out.printf("The area of the rectangle is: %.2f\n", rect.area());
+        System.out.printf("The perimeter of the rectangle is: %.2f", rect.perimeter());
+
+        sc.close();
+    }
+} */
