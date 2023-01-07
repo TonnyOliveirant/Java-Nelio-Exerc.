a482b417f099c3dd963ccdd83ba54fcb38ffd188
TonnyOliveirant <98629251+TonnyOliveirant@users.noreply.github.com>
2023-01-07 14:01:54 -0300

    Create Entities.java

---
+++ Entities.java
@@ -0,0 +1,35 @@
+/* package Entities;
+import java.lang.String;
+public class employee {
+    public String name;
+    public double salary;
+    public double tax;
+    public double percentageIncrement;
+
+    public double liquidsalary(){
+        return (salary - tax);
+    }
+    public double increasedsalary(){ 
+        return salary += (salary * percentageIncrement) - tax;
+    }
+}
+ */
+
+/* package Entities;
+
+public class Rectangle {
+    public double width;
+    public double height;
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+} */
